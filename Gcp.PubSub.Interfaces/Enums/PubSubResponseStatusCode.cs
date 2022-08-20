6ef4a771b8118645d46d55c82c8ea767625e16d6
Mir Newaz <mir.newaz@checkatrade.com>
2022-08-20 12:20:42 +0100

    init

---
+++ Gcp.PubSub.Interfaces/Enums/PubSubResponseStatusCode.cs
@@ -0,0 +1,10 @@
+﻿namespace Gcp.PubSub.Interfaces.Enums
+{
+    public enum PubSubResponseStatusCode
+    {
+        NotFound = 404,
+        OK = 200,
+        BadRequest = 400,
+        InternalServerError = 500
+    }
+}
